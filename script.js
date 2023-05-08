6349e7deb5842f3410bf569674d68c9ee848ae20
Mani Dodla <79918038+spraymachine@users.noreply.github.com>
2023-05-08 14:08:10 +0530

    added blob code

--- script.js
+++ script.js
@@ -4,3 +4,16 @@ window.addEventListener("scroll", function(){
     header.classList.toggle("sticky", window.scrollY > 0);
 });
 
+
+const blob = document.getElementById("blob");
+
+document.body.onpointermove = event => {
+
+    const { clientX, clientY} = event;
+
+    blob.animate({
+    left: `${clientX}px`,
+    top: `${clientY}px`
+
+    }, {duration: 3000, fill: "forwards"})
+}
